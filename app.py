7f4c6b76667305939495dcf51394c648610feafe
Binara <binarayasas2003@gmail.com>
2026-07-31 13:13:48 +0530

    App ready to production

--- app.py
+++ app.py
@@ -2,20 +2,41 @@ import subprocess
 import os
 import sys
 
+def resource_path(relative_path):
+
+    try:
+        base_path = sys._MEIPASS
+
+    except Exception:
+        base_path = os.path.abspath(".")
+
+    return os.path.join(
+        base_path,
+        relative_path
+    )
+
 
 def start_server():
-    server_path = os.path.join(
-        "server",
-        "flask_server.py"
+
+    server_path = resource_path(
+        os.path.join(
+            "server",
+            "flask_server.py"
+        )
     )
-    print("SMARTCARE FLASK SERVER\n" \
-    "+ Starting SmartCare Flask Server...")
+
+    print(
+        "SMARTCARE FLASK SERVER\n"
+        "+ Starting SmartCare Flask Server..."
+    )
+
     subprocess.run(
         [
             sys.executable,
             server_path
         ]
     )
+
 
 if __name__ == "__main__":
 
